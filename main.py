df4e57c88bcb9669c862c62baa2a9638c8838e09
Benedikt Hitz <benedikt.hitz@bfh.ch>
2024-03-28 07:41:21 +0000

    enable switching external data

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def index():
     # url parameter and their default values
     uri = request.args.get("uri", "https://ld.admin.ch/country/CHE")
     env = request.args.get("env", "prod")
+    ext = request.args.get("ext", "false")
     dir = request.args.get("dir", "nominal")
     limit = request.args.get("limit", "0")
     
@@ -48,41 +49,61 @@ def index():
     encoded_query = {"query": sparql_query}
 
     if env == "test":
-        sparql_endpoint_url = "https://test.ld.admin.ch/query"
+        lindas_enpoint_url = "https://test.ld.admin.ch/query"
     elif env == "int":
-        sparql_endpoint_url = "https://int.ld.admin.ch/query"
+        lindas_enpoint_url = "https://int.ld.admin.ch/query"
     else:
         env = "prod"
-        sparql_endpoint_url = "https://ld.admin.ch/query"
+        lindas_enpoint_url = "https://ld.admin.ch/query"
 
     headers = {
         "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
         "Accept": "application/sparql-results+json" 
     }
 
-    response1 = requests.post(sparql_endpoint_url, 
+    status = "nok"
+
+    response_lindas = requests.post(lindas_enpoint_url, 
                              data=encoded_query, 
                              headers=headers)
     
-    response2 = requests.post("http://graphdb.di.digisus-lab.ch/repositories/flader",
-                             data=encoded_query,
-                             headers=headers)
-    
-    response1.encoding = "utf-8"
-    response2.encoding = "utf-8"
 
-    if response1.status_code == 200 and response2.status_code == 200:
-        
-        data1 = response1.json()
-        data2 = response2.json()
+    if response_lindas.status_code == 200:
+
+        response_lindas.encoding = "utf-8"
+
+        data_lindas = response_lindas.json()
 
         # the variables of the query result
-        vars = data1["head"]["vars"]
+        vars = data_lindas["head"]["vars"]
 
         df = pd.DataFrame(columns = vars)
 
-        data = data1["results"]["bindings"] + data2["results"]["bindings"]
+        data = data_lindas["results"]["bindings"]
 
+        status = "ok"
+
+    if ext == "true":
+
+        response_ext = requests.post("http://graphdb.di.digisus-lab.ch/repositories/flader",
+                             data=encoded_query,
+                             headers=headers)
+        
+        if response_ext.status_code == 200:
+
+            response_ext.encoding = "utf-8"
+
+            data_ext = response_ext.json()
+
+            data = data + data_ext["results"]["bindings"]
+
+            status = "ok"
+
+        else:
+            status = "nok"
+
+    if status == "ok":
+        
         # for each line in the result table
         for line in data:
             line_list = []
@@ -92,7 +113,7 @@ def index():
                 
                 # if uri or blank node
                 if line[var]["type"] == "uri" or line[var]["type"] == "bnode":
-                    url = modify_uri(line[var]["value"], env, dir, limit)
+                    url = modify_uri(line[var]["value"], env, ext, dir, limit)
                     line_list.append(url)
                 
                 # if string literal
@@ -109,7 +130,7 @@ def index():
                         line_list.append(line[var]["value"] + " <span class='text-muted'>" + prefixer(line[var]["datatype"]) + "</span>")
                     else: #könnte trotzdem ein URL sein, der aber vom Server als Literal geschickt wird
                         #line_list.append(line[var]["value"])
-                        url = modify_uri(line[var]["value"], env, dir, limit)
+                        url = modify_uri(line[var]["value"], env, ext, dir, limit)
                         line_list.append(url)
 
                 else:
@@ -122,14 +143,14 @@ def index():
         # define all cells as markup so that the html code is properly displayed
         df = df.map(lambda x: Markup(x))
 
-        return render_template('dataframe.html', data=df, uri=uri, env=env, dir=dir, limit=limit, col_names=vars)
+        return render_template('dataframe.html', data=df, uri=uri, env=env, ext=ext, dir=dir, limit=limit, col_names=vars)
 
     else:
-        print(f"SPARQL query failed with status code: {response1.status_code} and response text: {response1.text}!")
+        print(f"status code lindas: {response_lindas.status_code} and response text lindas: {response_lindas.text}, status code ext: {response_ext.status_code} and response text ext: {resonse_ext.text}!")
         return render_template('template.html', uri=uri)
 
 # modifies the uris for correct linking (uri will be resolved with flader as long as possible)
-def modify_uri(input_string, env, dir, limit):
+def modify_uri(input_string, env, ext, dir, limit):
 
     # geo.ld.admin.ch and schema.ld.admin.ch are nor regularly dereferenced
     if input_string.startswith("https://geo.ld.admin.ch") or input_string.startswith("https://schema.ld.admin.ch"):
@@ -139,7 +160,7 @@ def modify_uri(input_string, env, dir, limit):
     pattern = r'^https://[^/]+\.ld\.admin\.ch'
     
     if input_string.startswith("https://ld.admin.ch") or re.match(pattern, input_string):
-        return "<a href='https://flader.di.digisus-lab.ch?uri=" + input_string + "&env=" + env + "&dir=" + dir + "&limit=" + limit + "'>" + input_string + "</a>"
+        return "<a href='https://flader.di.digisus-lab.ch?uri=" + input_string + "&env=" + env + "&ext=" + ext + "&dir=" + dir + "&limit=" + limit + "'>" + input_string + "</a>"
     
     # external URI
     if input_string.startswith("http://") or input_string.startswith("https://"):
@@ -147,7 +168,7 @@ def modify_uri(input_string, env, dir, limit):
     
     # blank node
     if input_string.startswith("genid"):
-        return "<a href='https://flader.di.digisus-lab.ch?uri=_:" + input_string + "&env=" + env + "&dir=" + dir + "&limit=" + limit + "'>" + input_string + "</a>"
+        return "<a href='https://flader.di.digisus-lab.ch?uri=_:" + input_string + "&env=" + env + "&ext=" + ext + "&dir=" + dir + "&limit=" + limit + "'>" + input_string + "</a>"
 
     # if string literal
     return input_string
